882d60c019898993db4ddb9603d7af3dcf8da611
adisiswoyo <adisiswoyo202@gmail.com>
2025-11-20 21:37:58 +0700

    add service logic PriceService

--- src/main/java/com/inventory/app/services/PriceService.java
+++ src/main/java/com/inventory/app/services/PriceService.java
@@ -1,4 +1,9 @@
 package com.inventory.app.services;
 
+import com.inventory.app.models.Price;
+
 public interface PriceService {
+    Price setPrice(Long variantId, Price price);
+
+    Price getByVariant(Long variantId);
 }
--- src/main/java/com/inventory/app/services/impl/PriceServiceImpl.java
+++ src/main/java/com/inventory/app/services/impl/PriceServiceImpl.java
@@ -1,6 +1,36 @@
 package com.inventory.app.services.impl;
 
+import com.inventory.app.models.Price;
+import com.inventory.app.models.Variant;
+import com.inventory.app.repository.PriceRepository;
+import com.inventory.app.repository.VariantRepository;
 import com.inventory.app.services.PriceService;
 
 public class PriceServiceImpl implements PriceService {
+
+    private final PriceRepository priceRepository;
+    private final VariantRepository variantRepository;
+
+    public PriceServiceImpl(PriceRepository priceRepository, VariantRepository variantRepository) {
+        this.priceRepository = priceRepository;
+        this.variantRepository = variantRepository;
+    }
+
+    @Override
+    public Price setPrice(Long variantId, Price price) {
+        Variant variant = variantRepository.findById(variantId)
+                .orElseThrow(() -> new RuntimeException("Variant not found"));
+
+        price.setVariant(variant);
+
+        return priceRepository.save(price);
+    }
+
+    @Override
+    public Price getByVariant(Long variantId) {
+        Variant variant = variantRepository.findById(variantId)
+                .orElseThrow(() -> new RuntimeException("Variant not found"));
+
+        return variant.getPrice();
+    }
 }
